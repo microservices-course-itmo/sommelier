4e287159f3f01176620b27dcd6eff65c1d10a261
Mixxx-a <34368295+Mixxx-a@users.noreply.github.com>
2020-09-30 18:52:26 +0300

    added new snapshot for ColorBox

--- src/theme/Colors/colorBox.test.tsx
+++ src/theme/Colors/colorBox.test.tsx
@@ -3,8 +3,12 @@ import renderer from 'react-test-renderer'
 import React from 'react'
 
 describe('ColorBox component', () => {
-  it('matches the snapshot', () => {
+  it('matches the snapshot white', () => {
     const tree = renderer.create(<ColorBox color={'#FFFFFF'} colorName={'white'}/>).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+  it('matches the snapshot black', () => {
+    const tree = renderer.create(<ColorBox color={'#000000'} colorName={'black'}/>).toJSON()
     expect(tree).toMatchSnapshot()
   })
 })
