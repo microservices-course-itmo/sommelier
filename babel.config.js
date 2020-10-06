9b437eef3eed96c284d70d04a06d4c5a058f8def
Mixxx-a <34368295+Mixxx-a@users.noreply.github.com>
2020-10-06 21:09:48 +0300

    Fixed jest!

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,6 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: [
-      'babel-preset-expo',
-      'module:metro-react-native-babel-preset',
-      'module:react-native-dotenv',
-      '@babel/preset-flow',
-    ],
+    presets: ['babel-preset-expo'],
   };
 };
