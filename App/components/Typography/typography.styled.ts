d2e2833727dfa1ca8cee88ebecbd65497d8f5159
Mixxx-a <34368295+Mixxx-a@users.noreply.github.com>
2020-10-16 23:56:17 +0300

    Added typography tests, modified typography props

--- App/components/Typography/typography.styled.ts
+++ App/components/Typography/typography.styled.ts
@@ -14,66 +14,59 @@ type TextProps = HeaderProps & {
 }
 
 export const StyledH1 = styled.Text<HeaderProps>`
-  color: ${({ color, theme }) => theme.colors[color]};
+  color: ${({ color }) => theme.colors[color]};
   font-size: 32px;
   font-weight: ${({ bold }) => (bold ? 'bold' : 'normal')};
   font-style: ${({ italic }) => (italic ? 'italic' : 'normal')};
-  ${({ fontFamily, theme }) =>
-    fontFamily && `font-family:${theme.fonts[fontFamily]}`};
+  ${({ fontFamily }) => fontFamily && `font-family:${theme.fonts[fontFamily]}`};
 `
 
 export const StyledH2 = styled.Text<HeaderProps>`
-  color: ${({ color, theme }) => theme.colors[color]};
+  color: ${({ color }) => theme.colors[color]};
   font-size: 24px;
   font-weight: ${({ bold }) => (bold ? 'bold' : 'normal')};
   font-style: ${({ italic }) => (italic ? 'italic' : 'normal')};
-  ${({ fontFamily, theme }) =>
-    fontFamily && `font-family:${theme.fonts[fontFamily]}`};
+  ${({ fontFamily }) => fontFamily && `font-family:${theme.fonts[fontFamily]}`};
 `
 
 export const StyledH3 = styled.Text<HeaderProps>`
-  color: ${({ color, theme }) => theme.colors[color]};
+  color: ${({ color }) => theme.colors[color]};
   font-size: 18.72px;
   font-weight: ${({ bold }) => (bold ? 'bold' : 'normal')};
   font-style: ${({ italic }) => (italic ? 'italic' : 'normal')};
-  ${({ fontFamily, theme }) =>
-    fontFamily && `font-family:${theme.fonts[fontFamily]}`};
+  ${({ fontFamily }) => fontFamily && `font-family:${theme.fonts[fontFamily]}`};
 `
 
 export const StyledH4 = styled.Text<HeaderProps>`
-  color: ${({ color, theme }) => theme.colors[color]};
+  color: ${({ color }) => theme.colors[color]};
   font-size: 16px;
   font-weight: ${({ bold }) => (bold ? 'bold' : 'normal')};
   font-style: ${({ italic }) => (italic ? 'italic' : 'normal')};
-  ${({ fontFamily, theme }) =>
-    fontFamily && `font-family:${theme.fonts[fontFamily]}`};
+  ${({ fontFamily }) => fontFamily && `font-family:${theme.fonts[fontFamily]}`};
 `
 
 export const StyledH5 = styled.Text<HeaderProps>`
-  color: ${({ color, theme }) => theme.colors[color]};
+  color: ${({ color }) => theme.colors[color]};
   font-size: 13.28px;
   font-weight: ${({ bold }) => (bold ? 'bold' : 'normal')};
   font-style: ${({ italic }) => (italic ? 'italic' : 'normal')};
-  ${({ fontFamily, theme }) =>
-    fontFamily && `font-family:${theme.fonts[fontFamily]}`};
+  ${({ fontFamily }) => fontFamily && `font-family:${theme.fonts[fontFamily]}`};
 `
 
 export const StyledH6 = styled.Text<HeaderProps>`
-  color: ${({ color, theme }) => theme.colors[color]};
+  color: ${({ color }) => theme.colors[color]};
   font-size: 10.72px;
   font-weight: ${({ bold }) => (bold ? 'bold' : 'normal')};
   font-style: ${({ italic }) => (italic ? 'italic' : 'normal')};
-  ${({ fontFamily, theme }) =>
-    fontFamily && `font-family:${theme.fonts[fontFamily]}`};
+  ${({ fontFamily }) => fontFamily && `font-family:${theme.fonts[fontFamily]}`};
 `
 
 export const StyledText = styled.Text<TextProps>`
-  color: ${({ color, theme }) => theme.colors[color]};
+  color: ${({ color }) => theme.colors[color]};
   font-weight: ${({ bold }) => (bold ? 'bold' : 'normal')};
   font-style: ${({ italic }) => (italic ? 'italic' : 'normal')};
-  ${({ fontFamily, theme }) =>
-    fontFamily && `font-family:${theme.fonts[fontFamily]}`};
-  font-size: ${({ size, theme }) => theme.fontSizes[size]};
+  ${({ fontFamily }) => fontFamily && `font-family:${theme.fonts[fontFamily]}`};
+  font-size: ${({ size }) => theme.fontSizes[size]};
   text-decoration: ${({ crossedOut }) =>
     crossedOut ? 'line-through' : 'none'};
 `
--- App/components/Typography/typography.test.tsx
+++ App/components/Typography/typography.test.tsx
@@ -1,42 +1,73 @@
 import React from 'react'
 import { render } from '@testing-library/react-native'
 
+import { ThemeProvider } from 'styled-components'
 import { H1, H2, H3, H4, H5, H6, Text } from './typography'
 import theme from '../../theme'
 
-const defaultProps = {
-  fontFamily: theme.fonts.PtSans,
+const themeRender = (component: any) => {
+  return render(<ThemeProvider theme={theme}>{component}</ThemeProvider>)
 }
 
 describe('Typography component', () => {
   describe('Component matches snapshot', () => {
     it('Text renders correctly', () => {
-      const tree = render(<Text {...defaultProps}>abc 123</Text>).toJSON()
+      const tree = themeRender(<Text>Abc 123</Text>).toJSON()
       expect(tree).toMatchSnapshot()
     })
     it('H1 renders correctly', () => {
-      const tree = render(<H1 {...defaultProps}>abc 123</H1>).toJSON()
+      const tree = themeRender(<H1>Abc 123</H1>).toJSON()
       expect(tree).toMatchSnapshot()
     })
     it('H2 renders correctly', () => {
-      const tree = render(<H2 {...defaultProps}>abc 123</H2>).toJSON()
+      const tree = themeRender(<H2>Abc 123</H2>).toJSON()
       expect(tree).toMatchSnapshot()
     })
     it('H3 renders correctly', () => {
-      const tree = render(<H3 {...defaultProps}>abc 123</H3>).toJSON()
+      const tree = themeRender(<H3>Abc 123</H3>).toJSON()
       expect(tree).toMatchSnapshot()
     })
     it('H4 renders correctly', () => {
-      const tree = render(<H4 {...defaultProps}>abc 123</H4>).toJSON()
+      const tree = themeRender(<H4>Abc 123</H4>).toJSON()
       expect(tree).toMatchSnapshot()
     })
     it('H5 renders correctly', () => {
-      const tree = render(<H5 {...defaultProps}>abc 123</H5>).toJSON()
+      const tree = themeRender(<H5>Abc 123</H5>).toJSON()
       expect(tree).toMatchSnapshot()
     })
     it('H6 renders correctly', () => {
-      const tree = render(<H6 {...defaultProps}>abc 123</H6>).toJSON()
+      const tree = themeRender(<H6>Abc 123</H6>).toJSON()
       expect(tree).toMatchSnapshot()
+    })
+  })
+  describe('Props testing', () => {
+    it('color prop assigns correctly', () => {
+      const { getByText } = themeRender(<Text color='lightGray'>abc</Text>)
+      expect(getByText('abc').props.color).toBe('lightGray')
+    })
+    it('bold prop assigns correctly ', () => {
+      const { getByText } = themeRender(<Text bold>abc</Text>)
+      expect(getByText('abc').props.bold).toBe(true)
+    })
+    it('italic prop assigns correctly ', () => {
+      const { getByText } = themeRender(<Text italic>abc</Text>)
+      expect(getByText('abc').props.italic).toBe(true)
+    })
+    it('crossed-out prop assigns correctly ', () => {
+      const { getByText } = themeRender(<Text crossedOut>abc</Text>)
+      expect(getByText('abc').props.crossedOut).toBe(true)
+    })
+    it('crossed-out prop assigns correctly ', () => {
+      const { getByText } = themeRender(<Text crossedOut>abc</Text>)
+      expect(getByText('abc').props.crossedOut).toBe(true)
+    })
+    it('font-family prop assigns correctly ', () => {
+      const { getByText } = themeRender(<Text fontFamily='PtSans'>abc</Text>)
+      expect(getByText('abc').props.fontFamily).toBe('PtSans')
+    })
+    it('size prop assigns correctly ', () => {
+      const { getByText } = themeRender(<Text size='lg'>abc</Text>)
+      expect(getByText('abc').props.size).toBe('lg')
     })
   })
 })
