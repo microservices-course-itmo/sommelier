8136390366c63216b47d79a35cf69e7bb005ce41
StanislavMyakishev <masiff-s@live.ru>
2020-10-20 21:06:09 +0300

    [SOMMELIER-15] story knobs text changes

--- index.ts
+++ index.ts
@@ -2,4 +2,13 @@ export { Box } from './App/components/box'
 export { Button } from './App/components/Button'
 export { Image } from './App/components/Image'
 export { StarsRating } from './App/components/StarsRating'
-export { H1, H2, H3, H4, H5, H6, Text } from './App/components/typography'
+export {
+  H1,
+  H2,
+  H3,
+  H4,
+  H5,
+  H6,
+  Text,
+  Header,
+} from './App/components/typography'
