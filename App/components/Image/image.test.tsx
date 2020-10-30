b931bb9d429c957b9988d7c0f1b7bc3bfd583464
Mixxx-a <34368295+Mixxx-a@users.noreply.github.com>
2020-10-30 17:55:16 +0300

    Added Image tests, need answers to some questions

---
+++ App/components/Image/image.test.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { themeRender } from '../test-utils'
+import Image from './image'
+
+describe('Image component', () => {
+  it('Component matches snapshot', () => {
+    const tree = themeRender(
+      <Image image='https://i.pinimg.com/originals/53/54/81/535481ed597409956a8e1288987a4ae3.jpg' />
+    ).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+  it('Width prop assigns correctly', () => {
+    const elem = themeRender(
+      <Image
+        image='https://i.pinimg.com/originals/53/54/81/535481ed597409956a8e1288987a4ae3.jpg'
+        width={50}
+      />
+    )
+    expect(elem.toJSON().children[0].props.width).toBe(50)
+  })
+  it('Height prop assigns correctly', () => {
+    const elem = themeRender(
+      <Image
+        image='https://i.pinimg.com/originals/53/54/81/535481ed597409956a8e1288987a4ae3.jpg'
+        height={50}
+      />
+    )
+    expect(elem.toJSON().children[0].props.height).toBe(50)
+  })
+  it('Border prop assigns correctly', () => {
+    const elem = themeRender(
+      <Image
+        image='https://i.pinimg.com/originals/53/54/81/535481ed597409956a8e1288987a4ae3.jpg'
+        border='solid'
+      />
+    )
+    expect(elem.toJSON().children[0].props.border).toBe('solid')
+  })
+  it('Border radius prop assigns correctly', () => {
+    const elem = themeRender(
+      <Image
+        image='https://i.pinimg.com/originals/53/54/81/535481ed597409956a8e1288987a4ae3.jpg'
+        borderRadius={5}
+      />
+    )
+    console.log(elem.toJSON())
+    expect(elem.toJSON().props.borderRadius).toBe(5)
+  })
+})
