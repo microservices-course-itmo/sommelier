174e0f8c2f9f2230d06e94fcd486f6988c5210c9
Mixxx-a <34368295+Mixxx-a@users.noreply.github.com>
2020-10-31 11:34:18 +0300

    Added searchInput tests

--- App/components/SearchInput/input.test.tsx
+++ App/components/SearchInput/input.test.tsx
@@ -10,4 +10,81 @@ describe('SearchInput component', () => {
     ).toJSON()
     expect(tree).toMatchSnapshot()
   })
+  it('default value prop assigns correctly', () => {
+    const elem = themeRender(
+      <SearchInput
+        defaultValue='abc'
+        editable
+        size='sm'
+        onChange={mockFn}
+        clearButtonMode
+      />
+    )
+    expect(elem.toJSON().children[1].props.defaultValue).toBe('abc')
+  })
+  it('placeholder prop assigns correctly', () => {
+    const elem = themeRender(
+      <SearchInput
+        placeholder='abc'
+        editable
+        size='sm'
+        onChange={mockFn}
+        clearButtonMode
+      />
+    )
+    expect(elem.toJSON().children[1].props.placeholder).toBe('abc')
+  })
+  it('ediatable prop assigns correctly', () => {
+    const elem = themeRender(
+      <SearchInput
+        editable={false}
+        size='sm'
+        onChange={mockFn}
+        clearButtonMode
+      />
+    )
+    expect(elem.toJSON().children[1].props.editable).toBeFalsy()
+  })
+  it('size prop assigns correctly', () => {
+    const elem = themeRender(
+      <SearchInput editable size='lg' onChange={mockFn} clearButtonMode />
+    )
+    expect(elem.toJSON().children[1].props.size).toBe('lg')
+  })
+  it('max length prop assigns correctly', () => {
+    const elem = themeRender(
+      <SearchInput
+        editable
+        size='sm'
+        maxLength={20}
+        onChange={mockFn}
+        clearButtonMode
+      />
+    )
+    expect(elem.toJSON().children[1].props.maxLength).toBe(20)
+  })
+  it('value prop assigns correctly', () => {
+    const elem = themeRender(
+      <SearchInput
+        editable
+        size='sm'
+        value='abc'
+        onChange={mockFn}
+        clearButtonMode
+      />
+    )
+    expect(elem.toJSON().children[1].props.value).toBe('abc')
+  })
+  it('clear button mode prop assigns correctly', () => {
+    const elem = themeRender(
+      <SearchInput
+        editable
+        size='sm'
+        onChange={mockFn}
+        clearButtonMode={false}
+      />
+    )
+    expect(elem.toJSON().children[1].props.clearButtonMode).toBeFalsy()
+  })
+  // OnChange()
 })
