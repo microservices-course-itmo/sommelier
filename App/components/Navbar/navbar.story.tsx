cc05b33bfa69621c2befad246a7e7947a2586b35
jnannni <catya.ladokhina@gmail.com>
2020-11-09 19:26:17 +0300

    deleted unused prop

--- App/components/Navbar/navbar.story.tsx
+++ App/components/Navbar/navbar.story.tsx
@@ -1,39 +1,35 @@
 import React from 'react'
-import Item from './navItem'
-import Navbar from './navbar'
 import { storiesOf } from '@storybook/react-native'
 import { action } from '@storybook/addon-actions'
+import { text } from '@storybook/addon-knobs'
 import { BufferView, ThemeDecorator } from '../../config/decorators'
 import MenuItem1 from '../../../assets/images/MenuItem1.svg'
 import MenuItem2 from '../../../assets/images/MenuItem2.svg'
 import MenuItem3 from '../../../assets/images/MenuItem3.svg'
 import MenuItem4 from '../../../assets/images/MenuItem4.svg'
-import { text } from '@storybook/addon-knobs'
+import Item from './navItem'
+import Navbar from './navbar'
 
 const defaultNavbar = () => (
   <Navbar>
     <Item
       onPress={action('tapper-default')}
-      selected={true}
       text={text('main', 'Главное')}
       icon={<MenuItem1 />}
     />
 
     <Item
       onPress={action('tapper-default')}
-      selected={false}
       text={text('catalog', 'Каталог')}
       icon={<MenuItem2 />}
     />
     <Item
       onPress={action('tapper-default')}
-      selected={false}
       text={text('favorite', 'Избранное')}
       icon={<MenuItem3 />}
     />
     <Item
       onPress={action('tapper-default')}
-      selected={false}
       text={text('profile', 'Профиль')}
       icon={<MenuItem4 />}
     />
