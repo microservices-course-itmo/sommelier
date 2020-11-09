fb4acced6816885a583b2f792b8e88358c2e4580
m.pavlov <m.pavlov@okko.tv>
2020-11-09 19:35:51 +0300

    some fixes

--- App/components/Switch/switch.styled.ts
+++ App/components/Switch/switch.styled.ts
@@ -9,15 +9,16 @@ export const Toggle = styled.View<ToggleProps>`
   height: 16px;
   width: 34px;
   border-radius: 50px;
-  background-color: ${({ value }) =>
-    value ? 'rgba(69, 208, 158, 0.5)' : '#ededed'};
+  background-color: ${({ value, theme }) =>
+    value ? theme.colors.switchToggleOn : theme.colors.switchToggleOff};
 `
 
 export const Notch = styled.View<NotchProps>`
   height: 20px;
   width: 20px;
   border-radius: 10px;
-  background-color: ${({ value }) => (value ? '#45d09e' : '#e0e0e0')};
+  background-color: ${({ value, theme }) =>
+    value ? theme.colors.switchNotchOn : theme.colors.switchNotchOff};
   ${({ value }) => (value ? 'right: -3px;' : 'left: -3px;')}
   top: -2px;
   position: absolute;
