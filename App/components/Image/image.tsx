001886d159281818e43130a15a3d06d583abafdf
StanislavMyakishev <masiff-s@live.ru>
2020-11-12 18:29:25 +0300

    removed redundant props

--- App/components/Image/image.tsx
+++ App/components/Image/image.tsx
@@ -2,14 +2,11 @@ import React from 'react'
 import * as Styled from './image.styled'
 import Box from '../Box/box'
 import { BoxProps } from '../Box/box.types'
-import theme from '../../theme'
 
 export type Props = {
   image: string
   width?: number
   height?: number
-  borderRadius?: number
-  borderColor?: keyof typeof theme.colors
 } & BoxProps
 
 const Image = (props: Props) => {
